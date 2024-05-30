8e0e92ed72408390f5ab4b93316d9607049acf43
NaoNerfaMeuQ <marcosalmeidac603@gmail.com>
2024-05-30 18:57:42 -0300

    1.8
    Menu colocado, imagens alinhadas, tabelas limitadas ao div, mudança de icon de pesquisa e centralizado a barra de pesquisa.
    
    A fazer: Terminar as tags com ajax
    - arrumar o filtro de petições por nome
    - fazer registro de petição
    - fazer página para olhar a petição

--- petitions/petitions.js
+++ petitions/petitions.js
@@ -2,7 +2,7 @@
 const checkbox = document.getElementById("checkbox");
 const menuOptions = document.querySelector(".menu-options");
 
-checkbox.addEventListener("change", function(menu) {
+checkbox.addEventListener("change", function() {
     if (this.checked) {
         menuOptions.style.display = "block"; // Exibe as opções
     } else {
@@ -10,25 +10,41 @@ checkbox.addEventListener("change", function(menu) {
     }
 });
 
-const input = document.getElementById('searchInput');
-    const table = document.getElementById('tituloinfo');
+const input = document.getElementById('infopeti');
+const table = document.getElementById('tabelapeti');
 
-    input.addEventListener('keyup', function() {
-        const filter = input.value.toLowerCase();
-        const rows = table.getElementsByTagName('tr');
+input.addEventListener('keyup', function() {
+    const filter = input.value.toLowerCase();
+    const rows = table.getElementsByTagName('td');
 
-        for (let i = 0; i < rows.length; i++) {
-            const cells = rows[i].getElementsByTagName('td');
-            let found = false;
+    for (let i = 0; i < rows.length; i++) {
+        const cells = rows[i].getElementsByTagName('tr');
+        let found = false;
 
-            for (let j = 0; j < cells.length; j++) {
-                const cellText = cells[j].textContent || cells[j].innerText;
-                if (cellText.toLowerCase().indexOf(filter) > -1) {
-                    found = true;
-                    break;
-                }
+        for (let j = 0; j < cells.length; j++) {
+            const cellText = cells[j].textContent || cells[j].innerText;
+            if (cellText.toLowerCase().indexOf(filter) > -1) {
+                found = true;
+                break;
             }
-
-            rows[i].style.display = found ? '' : 'none';
         }
-    });
+
+        rows[i].style.display = found ? '' : 'none';
+    }
+});
+
+
+
+    //Ajax petitions.html
+
+    function changepetition(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+           document.getElementById("infopeti").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "petitions.txt", true);
+        xhttp.send();
+      }
+    
